60dd4d235fcc71228bab7f2fa2f4e5e1066abcef
pratik4599 <aniketpatil74p@gmail.com>
2021-06-25 12:51:30 +0530

    cleaned code only necessary stuff kept

--- app.py
+++ app.py
@@ -4,42 +4,39 @@ from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
-# to be changed manually once a month
-# exp_date = '2021-06-24'
+# stuff that needs to be manually changed
+# in show.html need to change month in each table
+exp_date_zerodha = '2021-07-29'  
 
 
-def create_url(row):
+
+def create_url_for_options(row):
     final_url = "https://kite.zerodha.com/chart/ext/tvc/NFO-OPT/" + \
         row['tradingsymbol'] + "/" + str(row['instrument_token'])
     return final_url
 
-# filename = r'C:\Users\91956\Desktop\instrument.csv'
-# df = pd.read_csv(filename)
 df = pd.read_csv('https://api.kite.trade/instruments')
+# df1 is for futures
 df1 = df
 df = df[(df['segment'].str.contains("NFO-OPT") == True)]
-# df = df[df['instrument_type'].str.contains("CE") == True]
 df.drop(df[df['name'] == 'NIFTY'].index, inplace=True)
 df.drop(df[df['name'] == 'BANKNIFTY'].index, inplace=True)
 df.drop(df[df['name'] == 'FINNIFTY'].index, inplace=True)
 ls = (df.expiry.unique())
 print(ls[0])
 # exp_date = ls[0]
-exp_date = '2021-07-29'
-df.drop(df[df['expiry'] != exp_date].index, inplace=True)
-df['url'] = df.apply(lambda row: create_url(row), axis=1)
-# df.to_csv(r'C:\Users\91956\Desktop\a.csv')
+df.drop(df[df['expiry'] != exp_date_zerodha].index, inplace=True)
+df['url'] = df.apply(lambda row: create_url_for_options(row), axis=1)
 
 
-def create_url2(row):
+def create_url_for_futures(row):
     final_url = "https://kite.zerodha.com/chart/ext/tvc/NFO-FUT/" + \
         row['tradingsymbol'] + "/" + str(row['instrument_token'])
     return final_url
 
 df1 = df1[(df1['segment'].str.contains("NFO-FUT") == True)]
-exp_date = '2021-07-29'
-df1.drop(df1[df1['expiry'] != exp_date].index, inplace=True)
-df1['url'] = df1.apply(lambda row: create_url2(row), axis=1)
+df1.drop(df1[df1['expiry'] != exp_date_zerodha].index, inplace=True)
+df1['url'] = df1.apply(lambda row: create_url_for_futures(row), axis=1)
 
 
 
